f52ba5a81f430afc8c6c26a9e0ef6058fe9ef991
leidybm <38796567+leidybm@users.noreply.github.com>
2018-04-28 19:31:21 -0500

    se sube fold proyecto frontend

---
+++ frontend/includes/head.php
@@ -0,0 +1,10 @@
+<head>
+  <title>Desarrollo Leidy</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="librerias/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+  <link href="librerias/css/w3.css" rel="stylesheet" type="text/css"/>
+  <script src="librerias/js/bootstrap.min.js" type="text/javascript"></script>
+  <script src="librerias/js/jquery-3.1.1.min.js" type="text/javascript"></script>
+  <link href="librerias/css/mi_estilo.css" rel="stylesheet" type="text/css"/>
+</head>

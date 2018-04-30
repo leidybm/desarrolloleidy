2ac6d91dc558690826183965d0f2a06d5e5a52b0
leidybm <38796567+leidybm@users.noreply.github.com>
2018-04-30 10:38:50 -0500

    modificación de archivo conexión

---
+++ backend/conexion/conexion.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost";
+$user = "********";
+$pass = "********";
+$bd = "modelo_bd_leidy";
+
+//Creamos la conexión
+$conexion = mysqli_connect($server, $user, $pass,$bd) 
+or die("Ha sucedido un error inexperado en la conexion de la base de datos");

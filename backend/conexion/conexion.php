e99260c905eb2cacb7ee680f4c4c83fe591496f2
leidybm <38796567+leidybm@users.noreply.github.com>
2018-04-30 10:35:09 -0500

    archivo con falla

--- backend/conexion/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server = "X.X.X.X";
-$user = "xxxxxxx";
-$pass = "xxxxxxxxxx";
-$bd = "modelo_bd_leidy";
-
-//Creamos la conexión
-$conexion = mysqli_connect($server, $user, $pass,$bd) 
-or die("Ha sucedido un error inexperado en la conexion de la base de datos");

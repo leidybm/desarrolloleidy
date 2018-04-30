fb7b170e4fd16d593ba48a1e9963477a80cbc6a0
leidybm <38796567+leidybm@users.noreply.github.com>
2018-04-30 10:38:24 -0500

    Delete conexion.php

--- backend/conexion/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server = "X.X.X.X";
-$user = "xxxxxx";
-$pass = "@DM1N1STR4D0R2016";
-$bd = "modelo_bd_leidy";
-
-//Creamos la conexión
-$conexion = mysqli_connect($server, $user, $pass,$bd) 
-or die("Ha sucedido un error inexperado en la conexion de la base de datos");
